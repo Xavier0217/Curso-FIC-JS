5d64cb6899e3c4f62b8a5d2537d6e06e307919af
Xavier0217 <arthur.xavier016002@gmail.com>
2024-06-24 08:57:13 -0300

    aula01

---
+++ aula01/if.js
@@ -0,0 +1,12 @@
+// if em js
+
+const aluno = "Alfredo José"
+const nota1 = 8.9
+const nota2 = 3.4
+const media = (nota1 + nota2) / 2
+
+if (media >= 6) {
+    // aprovado
+} else {
+    // reprovado
+}
---
+++ aula01/variaveis.js
@@ -0,0 +1,13 @@
+// variaveis do js
+
+var curso = "Progamando em JS"
+let escola = "Senai"
+const cidade = "Piracicaba"
+
+curso = "Programação Web"
+escola = "Senai Vila"
+// cidade = "Piracaia"
+
+console.log(curso)
+console.log(escola)
+console.log(cidade)
